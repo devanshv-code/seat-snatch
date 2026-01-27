34148831635a729e073dcc9a2a78d8f50e287755
devanshv-code <devanshv001@gmail.com>
2026-01-27 15:23:33 +0530

    Add more Attributes

--- src/Booking.java
+++ src/Booking.java
@@ -1,11 +1,18 @@
 public class Booking {
     private int booking_id;
     private int contact_no;
+    Ride ride;
+    int seatBooked;
+    User user;
+    double totalFare;
 
-
-    public Booking(int booking_id, int contact_no) {
-        this.booking_id = booking_id;
+    public Booking(int contact_no, int booking_id, Ride ride, int seatBooked, User user, double totalFare) {
         this.contact_no = contact_no;
+        this.booking_id = booking_id;
+        this.ride = ride;
+        this.seatBooked = seatBooked;
+        this.user = user;
+        this.totalFare = totalFare;
     }
 
     public int getBooking_id() {
@@ -23,4 +30,7 @@ public class Booking {
     public void setContact_no(int contact_no) {
         this.contact_no = contact_no;
     }
+
+
+
 }
